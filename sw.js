5597a4122161010eb7912b4cf656df50bb4d7101
8w2jh948cw-sudo <8w2jh948cw@privaterelay.appleid.com>
2026-08-30 08:23:39 -0300

    Atualiza cache para visualização segura dos registros

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='registro-v7-0.4.2';
+const CACHE='registro-v8-0.4.3';
 const FILES=[
   './',
   './index.html',
@@ -14,6 +14,7 @@ const FILES=[
   './v04c7.js?v=0.4.0',
   './v04c8.js?v=0.4.0',
   './v04c9.js?v=0.4.0',
+  './v04c10.js?v=0.4.3',
   './v04demo.js?v=0.4.1',
   './v04tabbar.js?v=0.4.2',
   './manifest.webmanifest?v=0.4.0',
