7017466d6dcca889d90f8bc3653e890011b54ef1
8w2jh948cw-sudo <8w2jh948cw@privaterelay.appleid.com>
2026-08-30 19:09:25 -0300

    Atualiza cache da escala de humor saturada

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='registro-v20-0.4.15';
+const CACHE='registro-v21-0.4.16';
 const FILES=[
   './',
   './index.html',
@@ -21,6 +21,7 @@ const FILES=[
   './v04c14.js?v=0.4.7',
   './v04c15.js?v=0.4.14',
   './v04c16.js?v=0.4.11',
+  './v04c17.js?v=0.4.16',
   './v04demo.js?v=0.4.1',
   './v04tabbar.js?v=0.4.2',
   './v04navicons.js?v=0.4.10',
